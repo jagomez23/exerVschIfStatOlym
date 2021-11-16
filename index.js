833e2f35955faf0f1166ee99f6d3e098c3802e75
jagomez23 <u0460391@umail.utah.edu>
2021-11-16 14:27:38 -0700

    Gold Medal Attempt

---
+++ index.js
@@ -0,0 +1,62 @@
+
+//Preliminaries
+
+if( 5 > 3 ){
+    console.log("is greater than")
+}
+
+if( "cat".length === 3 ){
+    console.log("is the length")
+}
+
+if( "cat" === "dog"){
+} else{
+    console.log ("not the same")
+}
+
+//Bronze Medal
+
+var person = {
+    name: "Bobby",
+    age: 12
+  };
+
+if(person.age >= 18){
+      console.log("Bobby is allowed to go to the movie")
+}else{
+      console.log("Bobby is NOT allowed to go to the movie, Sorry!")
+}
+
+if(person.name[0] === "B" || person.age !=18){
+    console.log("Bobby is allowed to go to the movie")
+}else {
+    console.log("Bobby is NOT allowed to go to the movie, Sorry!")
+};
+
+
+if(person.name[0] === "B" && person.age >=18){
+    console.log("Bobby is allowed to go to the movie")
+}else {
+    console.log("Bobby is NOT allowed to go to the movie, Sorry!")
+}
+
+//Silver Medal
+
+if(1 === "1"){
+    console.log("strict")
+}else if(1 == "1"){
+    console.log("loose")
+}else{
+    console.log("not equal at all")
+}
+if(1 <=2 && 2 === 4){
+    console.log("yes")
+}
+
+
+//Gold Medal
+
+var dog = "dog"
+if(typeof dog === "string"){
+    console.log(true)
+}
